648834af8b35ab1caee05f0416b77a6b6bfa9506
Stephen OBrien <stephen.obrien4@emc.com>
2017-07-02 11:07:00 +0100

    Add scripts to gen cert assets

---
+++ tls/gen_assets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p {ca,apiserver,admin}
+
+# Cluster CA
+pushd ca
+openssl genrsa -out ca-key.pem 2048
+openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
+popd
+
+# Create apiserver keypair
+pushd apiserver
+openssl genrsa -out apiserver-key.pem 2048
+openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config ../openssl.cnf
+openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile ../openssl.cnf
+popd
+
+# Cluster admin keypair
+pushd admin
+openssl genrsa -out admin-key.pem 2048
+openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin"
+openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
+popd
---
+++ tls/gen_worker_assets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p worker
+
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WORKER_FQDN=$1
+WORKER_IP=$2
+
+# Worker keypairs
+pushd worker
+openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
+WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config ../worker-openssl.cnf
+WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ../ca/ca.pem -CAkey ../ca/ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile ${CUR_DIR}/worker-openssl.cnf
+popd
+
